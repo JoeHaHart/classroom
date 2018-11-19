5e8b7d2df3263ba51ba57d8b0d59cee232cdeffb
Joe Hart <joehart42@gmail.com>
2018-11-19 19:09:23 +0000

    Add destory logic and correct spacing of blocks.

--- Assets/DestroyOnFloor.cs
+++ Assets/DestroyOnFloor.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyOnFloor : MonoBehaviour {
+	public Transform respawnLocation;
 
-	private void OnCollisionEnter(Collision other) {
-		Debug.Log(other.gameObject.name);
-		if (other.gameObject.name == "Floor") {
-			Debug.Log("Oooh I hit the floor!");
-		}
-	}
+void OnCollisionEnter (Collision col)
+ {   
+	Debug.Log(col.gameObject.name);
+        if(col.gameObject.name == "Floor")
+        {
+            this.gameObject.transform.position = respawnLocation.position;
+			this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    
+}
 }
