30546938ce177afaa18545f6a4d2f353b9fe3761
Joe Hart <joehart42@gmail.com>
2019-01-03 11:01:52 +0000

    set up time management and generic collider

---
+++ Assets/CollisionEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class CollisionEvent : MonoBehaviour {
+	public UnityEvent triggerEvent;
+	public string NameOfGameObject;
+
+	// Update is called once per frame
+    void OnCollisionEnter (Collision col) {
+        Debug.Log (col.gameObject.name);
+        if (col.gameObject.name == NameOfGameObject) {
+			Debug.Log("Trigger");
+            triggerEvent.Invoke();
+        } else {
+			Debug.Log("Name " + col.gameObject.name + " not equal to " + NameOfGameObject);
+		}
+
+    }
+}
