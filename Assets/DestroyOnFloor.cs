40932971fff16480eb2cd9871d8ea48947d18a6c
Joe Hart <joehart42@gmail.com>
2019-01-03 10:13:05 +0000

    Testing autofformatting and committing on new machine

--- Assets/DestroyOnFloor.cs
+++ Assets/DestroyOnFloor.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyOnFloor : MonoBehaviour {
-	public Transform respawnLocation;
+    public Transform respawnLocation;
 
-void OnCollisionEnter (Collision col)
- {   
-	Debug.Log(col.gameObject.name);
-        if(col.gameObject.name == "Floor")
-        {
+    void OnCollisionEnter (Collision col) {
+        Debug.Log (col.gameObject.name);
+        if (col.gameObject.name == "Floor") {
             this.gameObject.transform.position = respawnLocation.position;
-			this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            this.GetComponent<Rigidbody> ().velocity = Vector3.zero;
         }
-    
-}
+
+    }
 }
