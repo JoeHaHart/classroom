307cdd97d53edbfaf4a2c8b42b1ab14e782eb260
Joe Hart <joehart42@gmail.com>
2019-01-27 14:14:11 +0000

    Game Logic additions

--- Assets/QuestionManager.cs
+++ Assets/QuestionManager.cs
@@ -2,20 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestionManager : MonoBehaviour {
-	public string QuestionText;
-	public GameObject questionPrefab;
-	private GameObject questionInstance;
-	public Transform questionSpawnLocation;
+public class QuestionManager : MonoBehaviour
+{
+    public string QuestionText;
+    public GameObject questionPrefab;
+    private GameObject questionInstance;
+    public Transform questionSpawnLocation;
+    private VectorSelector vectorSelector;
+    public GameObject vectorSelectorPrefab;
+    Transform JoeJeffTransform;
 
-	public void askQuestion () {
-		questionInstance = Instantiate (questionPrefab, questionSpawnLocation.position, questionSpawnLocation.rotation);
-		questionInstance.GetComponent<TextMesh>().text = QuestionText;
-	}
+    void Start()
+    {
+        JoeJeffTransform = GameObject.Find("JoeJeff").transform;
+    }
 
-	public void answerQuestion() {
-		if (questionInstance != null) {
-			Destroy(questionInstance);
-		}
-	}
+
+    public void askQuestion()
+    {
+        questionInstance = Instantiate(questionPrefab, questionSpawnLocation.position, questionSpawnLocation.rotation);
+        questionInstance.GetComponent<TextMesh>().text = QuestionText;
+        GameObject selector = Instantiate(vectorSelectorPrefab, JoeJeffTransform);
+        vectorSelector = selector.GetComponent<VectorSelector>();
+    }
+
+
+    public void answerQuestion()
+    {
+        if (questionInstance != null)
+        {
+            Destroy(questionInstance);
+        }
+    }
 }
