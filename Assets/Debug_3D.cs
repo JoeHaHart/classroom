8574b510402311c2db84cc28891817e95e781c7f
Joe Hart <joehart42@gmail.com>
2019-01-27 15:23:46 +0000

    Set scale to single value and add positional offset to update function

--- Assets/Debug_3D.cs
+++ Assets/Debug_3D.cs
@@ -2,30 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Debug_3D : MonoBehaviour {
-	public bool localDebug = false;	
-	public GameObject debugTextPrefab;
-	private GameObject text;
-	public Transform lookTarget;
-	public string prefix;
-	public Vector3 rotationOffset;
-	public Vector3 positionOffset;
-	public float scale = 1.0f;
+public class Debug_3D : MonoBehaviour
+{
+    public bool localDebug = false;
+    public GameObject debugTextPrefab;
+    private GameObject text;
+    public Transform lookTarget;
+    public string prefix;
+    public Vector3 rotationOffset;
+    public Vector3 positionOffset;
+    float scale = 1.0f;
 
-	void Start() {
-		if (localDebug) {
-			text = Instantiate(debugTextPrefab, transform.position + positionOffset, transform.rotation);
-			text.transform.localScale = new Vector3(scale, scale, scale);
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(localDebug) {
-			text.GetComponent<TextMesh>().text = prefix + ": " + transform.localEulerAngles.ToString();
-			text.transform.position = transform.position;
-			text.transform.LookAt(lookTarget);
-			text.transform.Rotate(rotationOffset);
-		}
-	}
+    void Start()
+    {
+        if (localDebug)
+        {
+            text = Instantiate(debugTextPrefab, transform.position + positionOffset, transform.rotation);
+            text.transform.localScale = Vector3.one * scale;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (localDebug)
+        {
+            text.GetComponent<TextMesh>().text = prefix + ": " + transform.localEulerAngles.ToString();
+            text.transform.position = transform.position + positionOffset;
+            text.transform.LookAt(lookTarget);
+            text.transform.Rotate(rotationOffset);
+        }
+    }
 }
