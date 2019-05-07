a02a9b13896fb6e344c7c2389e889a8755de65dd
Joe Hart <joehart42@gmail.com>
2019-05-07 19:19:32 +0100

    tidy

---
+++ Assets/ResetLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetLevel : MonoBehaviour {
+    private float GRAVITY = 9.81f;
+    private Vector3 customGravity;
+
+	// Use this for initialization
+	void Start () {
+		customGravity = new Vector3(0, -GRAVITY, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("p")){
+			Application.LoadLevel(0);
+			Physics.gravity = customGravity;
+
+		}
+	}
+}
